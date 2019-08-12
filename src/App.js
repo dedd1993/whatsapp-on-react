5e5e7ad99891b31a39640f743e6c98c1688de630
Daniel <dedd1993@gmail.com>
2019-08-11 19:49:55 -0500

    feat: change app theme

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header";
 
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#245e54"
+    },
+    secondary: {
+      light: "#fff",
+      main: "#62c75c"
+    }
+  }
+});
+
 function App() {
-  return <Header />;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Header />
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
